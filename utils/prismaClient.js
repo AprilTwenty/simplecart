08bc81f0dc5c7f15bae30c410dca49e278727ba2
April Twenty <atcahriya.nna@gmail.com>
2025-11-02 22:32:41 +0700

    remove prismaClient.js

--- utils/prismaClient.js
+++
@@ -1,9 +0,0 @@
-import { execSync } from "child_process";
-try {
-  execSync("npx prisma generate");
-} catch (e) {
-  console.log("⚠️ Failed to auto-generate Prisma Client", e);
-}
-
-import { PrismaClient } from '@prisma/client';
-export const prisma = new PrismaClient();
