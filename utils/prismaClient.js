efb874121fbacf74f20c7238d89e4fd3f23465b0
April Twenty <atcahriya.nna@gmail.com>
2025-11-02 21:59:49 +0700

    fix: add run generate on runtime

--- utils/prismaClient.js
+++ utils/prismaClient.js
@@ -1,2 +1,9 @@
+import { execSync } from "child_process";
+try {
+  execSync("npx prisma generate");
+} catch (e) {
+  console.log("⚠️ Failed to auto-generate Prisma Client", e);
+}
+
 import { PrismaClient } from '@prisma/client';
 export const prisma = new PrismaClient();
